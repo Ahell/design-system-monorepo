d60a643e72a880304fd5de1e470764deabad5a49
Anders Hellström <ahell@kth.se>
2026-04-02 09:56:34 +0200

    chore: commit changes

--- apps/backflip/vite.config.js
+++ apps/backflip/vite.config.js
@@ -4,6 +4,11 @@ export default defineConfig({
   server: {
     port: 3020,
     host: true,
+    allowedHosts: [
+      'localhost',
+      '127.0.0.1',
+      'backflip.whcg.se',
+    ],
   },
   build: {
     outDir: "dist",
